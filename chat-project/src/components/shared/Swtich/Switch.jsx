9483c93d9b38f1a80b2c1b7704160c0e4417a376
jhkimon <sky06098i1@gmail.com>
2025-01-20 04:21:29 +0900

    feat: step4-2 headless ui

---
+++ chat-project/src/components/shared/Swtich/Switch.jsx
@@ -0,0 +1,16 @@
+import { Switch } from '@headlessui/react';
+import styles from './Switch.module.css';
+
+const CustomSwitch = ({ enabled, onChange }) => {
+    return (
+        <Switch
+            checked={enabled}
+            onChange={onChange}
+            className={`${styles.switch} ${enabled ? styles.switchEnabled : styles.switchDisabled}`}
+        >
+            <span className={styles.switchThumb} />
+        </Switch>
+    );
+};
+
+export default CustomSwitch;
--- chat-project/src/pages/Component/Component.jsx
+++ chat-project/src/pages/Component/Component.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/layout/Header/Header';
 import Footer from '../../components/layout/Footer/Footer';
 import Button from '../../components/shared/Button/Button';
 import MessageList from '../../components/MessageList/MessageList';
+import CustomCheckbox from '../../components/shared/CheckBox/CheckBox';
+import CustomSwitch from '../../components/shared/Swtich/Switch';
 import { faUser, faCog, faPlus } from '@fortawesome/free-solid-svg-icons';
 import styles from './Component.module.css';
 import { showAlert } from '../../utils/Alert';
@@ -16,6 +18,21 @@ const ComponentPage = () => {
         'How can I learn advanced React patterns?',
     ];
 
+    // useState로 checkbox의 상태 관리
+    const [checkboxChecked, setcheckboxChecked] = useState(false);
+    const handleCheckboxChange = (isChecked) => {
+        setcheckboxChecked(isChecked); // 상태 업데이트
+        console.log('Checkbox is now:', isChecked);
+    };
+
+    // useState로 Switch의 상태 관리
+    const [switchEnabled, setSwitchEnabled] = useState(false);
+
+    const handleSwitchChange = (isEnabled) => {
+        setSwitchEnabled(isEnabled); // 상태 업데이트
+        console.log('Switch is now:', isEnabled);
+    };
+
     const UserClick = () => {
         showAlert('안녕하세요! 유저 프로필입니다.');
     };
@@ -28,12 +45,17 @@ const ComponentPage = () => {
         <div className={styles['page-container']}>
             <Header />
             <main className={styles.content}>
-                Content goes here
                 <Button label="User Profile" onClick={UserClick} />
                 <Button label="Begin a New Chat" onClick={ChatClick} />
                 <Button label="User Profile" onClick={UserClick} leftIcon={faUser} rightIcon={faCog} />
                 <Button label="Begin a New Chat" onClick={ChatClick} rightIcon={faPlus} />
                 <MessageList messages={messages} />
+                <CustomCheckbox
+                    checked={checkboxChecked}
+                    onChange={handleCheckboxChange}
+                    label="Big Kahuna Burger Ltd."
+                />
+                <CustomSwitch enabled={switchEnabled} onChange={handleSwitchChange} />
             </main>
             <Footer />
         </div>
