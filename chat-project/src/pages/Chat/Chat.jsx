a650bf65583278a7de5114cb6aa88331d5546931
jhkimon <sky06098i1@gmail.com>
2025-01-25 15:53:16 +0900

    feat: step5: sidebar

--- chat-project/src/pages/Chat/Chat.jsx
+++ chat-project/src/pages/Chat/Chat.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import './Chat.module.css';
+import Button from '../../components/shared/Button/Button';
+import MessageList from '../../components/MessageList/MessageList';
+import { faUser, faCog, faPlus } from '@fortawesome/free-solid-svg-icons';
+import styles from './Chat.module.css';
 
-const Chat = () => {
+const ChatPage = () => {
+    const messages = [
+        'How can I increase the number of users?',
+        "What's the best approach to handle errors?",
+        "How can I improve my app's performance?",
+        'What are the latest trends in web development?',
+        'How can I learn advanced React patterns?',
+    ];
+
+    const ChatClick = () => {
+        alert('새로운 채팅을 시작할까요?');
+    };
+
+    const UserClick = () => {
+        alert('안녕하세요! 유저 프로필입니다.');
+    };
+
     return (
-        <div>
-            <h1>Chat</h1>
+        <div className={styles.container}>
+            <div className={styles.headerSection}>
+                <Button label="Begin a New Chat" onClick={ChatClick} rightIcon={faPlus} />
+                <MessageList messages={messages} />
+            </div>
+            <div className={styles.footerSection}>
+                <Button label="User Profile" onClick={UserClick} leftIcon={faUser} rightIcon={faCog} />
+            </div>
         </div>
     );
 };
 
-export default Chat;
+export default ChatPage;
