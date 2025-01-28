0cb4d2e1e944019bc3825100636bdb9447db2eef
jhkimon <sky06098i1@gmail.com>
2025-01-28 09:52:13 +0900

    fix: message item onclick

--- chat-project/src/components/MessageList/MessageList.jsx
+++ chat-project/src/components/MessageList/MessageList.jsx
@@ -1,12 +1,12 @@
 import styles from './MessageList.module.css';
 import MessageItem from '../MessageItem/MessageItem';
 
-const MessageList = ({ messages }) => {
+const MessageList = ({ messages, setSelectedChat }) => {
     return (
         <div className={styles['message-list']}>
             <div className={styles['message-title']}>Recent Chats</div>
             {messages.map((message, index) => (
-                <MessageItem key={index} message={message} />
+                <MessageItem key={index} message={message} onClick={() => setSelectedChat(index)} />
             ))}
         </div>
     );
