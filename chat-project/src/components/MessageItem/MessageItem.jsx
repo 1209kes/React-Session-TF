7c6311c18197977779e6135d7a7b035b34c70cce
jhkimon <sky06098i1@gmail.com>
2025-01-28 09:57:03 +0900

    fix: sync with session: message item onclick

--- chat-project/src/components/MessageItem/MessageItem.jsx
+++ chat-project/src/components/MessageItem/MessageItem.jsx
@@ -2,9 +2,9 @@ import styles from './MessageItem.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMessage } from '@fortawesome/free-solid-svg-icons';
 
-const MessageItem = ({ message }) => {
+const MessageItem = ({ message, onClick }) => {
     return (
-        <div className={styles['message-item']}>
+        <div className={styles['message-item']} onClick={onClick}>
             <FontAwesomeIcon icon={faMessage} className={styles['message-icon']} />
             <span className={styles['message-text']}>{message}</span>
         </div>
